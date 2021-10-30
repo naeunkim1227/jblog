288171d396eb61fdcd0305b7bbe49808a5a53958
naeun <kne0140@daum.net>
2021-10-30 14:49:46 +0900

    21.10.30

--- jblog03/src/main/java/com/douzone/jblog/security/LoginInterceptor.java
+++ jblog03/src/main/java/com/douzone/jblog/security/LoginInterceptor.java
@@ -2,6 +2,7 @@ package com.douzone.jblog.security;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -17,12 +18,30 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
+		System.out.println("로그인 인터셉터 실행");
+		
 		String id = request.getParameter("id");
 		String password = request.getParameter("password");
 		
 		UserVo authUser = userService.login(id, password);
 		
-		return super.preHandle(request, response, handler);
+		if(authUser == null) {
+			System.out.println("회원 정보 없음");
+			
+			request.setAttribute("result", "fail");
+			request.getRequestDispatcher("/WEB-INF/views/user/login.jsp").forward(request, response);
+		
+			return false;
+		}
+		
+		
+		HttpSession session =request.getSession(true);
+		session.setAttribute("authUser", authUser);
+		
+		
+		response.sendRedirect(request.getContextPath());
+		
+		return false;
 	}
 	
 	
---
+++ jblog03/src/main/java/com/douzone/jblog/security/LogoutInterceptor.java
@@ -0,0 +1,29 @@
+package com.douzone.jblog.security;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.douzone.jblog.service.UserService;
+
+public class LogoutInterceptor extends HandlerInterceptorAdapter{
+	
+	@Autowired
+	UserService userService;
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		
+		
+		
+		
+		return false;
+	}
+	
+	
+	
+}
